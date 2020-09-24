9af6b8eb8b647ef01cbd6b98f56aa4316e3ad11c
nbeckley23 <nickbeckley@gmail.com>
2020-09-23 22:49:14 -0600

    added some logic for timing and velocities

--- SFML/src/game.cpp
+++ SFML/src/game.cpp
@@ -6,7 +6,8 @@
 //
 
 #include "game.hpp"
-
+#include <chrono>
+#include <ctime>
 //sets frame limit to 60fps
 const sf::Time Game::TimePerFrame = sf::seconds(1.f/60.f);
 
@@ -14,6 +15,7 @@ Game::Game():
 // makes a window that can only be closed and has a title bar; no resizing is allowed
 window(sf::VideoMode(1200, 1200), "Lunar Lander", sf::Style::Close | sf::Style::Titlebar)
 {
+    window.setFramerateLimit(60);
     Terrain terrain; // new terrain object
     terrain_points = terrain.get_terrain_points();
     Lander lander; // new lander object
@@ -25,29 +27,55 @@ window(sf::VideoMode(1200, 1200), "Lunar Lander", sf::Style::Close | sf::Style::
 
 void Game::Run(){
     
-    sf::Clock clock; // starts the clock
+    sf::Clock clock;
     sf::Time elapsed1 = clock.getElapsedTime();
-    
+
     while (window.isOpen()) {
+        
+        //clock_t begin = clock();
+        sf::Vector2f pos_beg = lander.sprite.getPosition();
+        
+        // starts the clock
         sf::Time elapsed = clock.getElapsedTime();
-        txt[1].val = elapsed.asSeconds(); // update time
+
+        txt[1].val = elapsed.asSeconds();// update time
         window.draw(terrain_points);
         drawTxt();
         window.display();
         processEvents();
+        //clock_t end = clock();
+        sf::Vector2f pos_end = lander.sprite.getPosition();
+        //double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
+        txt[4].val = (pos_end.x - pos_beg.x)/TimePerFrame.asSeconds();
+        txt[5].val = (pos_end.y - pos_beg.y)/TimePerFrame.asSeconds();
+        txt[3].val = 1200-(lander.sprite.getPosition().y);
+        
     }
 }
 
 //called every time the game is updated ~60 times a second
 void Game::processEvents(){
     sf::Event event;
+    
+
+    
        while (window.pollEvent(event))
        {
+
            switch (event.type) {
                case sf::Event::Closed: {
                   window.close();
                   break; }}
        }
+    clock_t begin = clock();
+    clock_t end = clock();
+    double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
+    
+    sf::Vector2f pos1 = lander.sprite.getPosition();
+    float offset_X = lander.h_vel * 0.0167;
+    float offset_Y = lander.v_vel * 0.0167;
+    lander.sprite.setPosition(pos1.x + offset_X, pos1.y + offset_Y);
+    
     
     sf::Vector2f pos = lander.sprite.getPosition();
     float offsetX = 0;
--- SFML/src/lander.cpp
+++ SFML/src/lander.cpp
@@ -4,14 +4,14 @@
 #include "lander.hpp"
 
 Lander::Lander(){
-    sf::Vector2f defaultPos = {200,50};
+    sf::Vector2f defaultPos = {600,50};
     texture.loadFromFile("../src/lunar_lander3.png"); // load lander image
     sprite.setColor(sf::Color::Red); // set lander color
     sprite.setScale(0.1f, 0.1f); // 10% scale
     sprite.setTexture(texture); // sprite is now lander image
     sprite.setPosition(defaultPos); // sets it towards top of screen
     h_vel = 0.0;
-    v_vel = 0.0;
+    v_vel = 30.0;
     pos = defaultPos;
     fuel = 1000;
     altitude = 1000;
